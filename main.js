6312e2e1463c42b2598729ee4e7f2a7a77ef8f50
Johnny Wright <johnnywrightiv@gmail.com>
2022-07-29 21:49:53 -0500

    fixed errors in logic, program now runs correctly

--- main.js
+++ main.js
@@ -1,43 +1,21 @@
 // Write a function that takes an array of integers, and finds the pair of adjacent elements that has the largest product and return that product.
 
-// NOTE: I just realized my logic is flawed in the way I tried to approach this problem. Saving in repo to look back on and learn from.
-
 
 // given
-var arrOfInts = [9, 5, 10, 2, 24, -1, -48]
-var multiplier = 0;
+var numbers = [1, 3, 5, 8];
+var products = [];
 
-// find largest element of array -- some sort of SORTing (no sorting, that changes the array... instead reduce!)
-function findLargestElement(arr, length) {
-    var largestElement = arr.reduce(function(a,b){
-        return Math.max(a, b);
-    });
-    return multiplier = largestElement;
-};
 
-// compare adjascent numbers to largest element to find larger of the two
-function findLargestAdjascentElement(arr) {
-    var indexOfLargest = arr.findIndex(element => element === multiplier);
-    // console.log(indexOfLargest);
-    var numberBefore = arr[indexOfLargest-1];
-    var numberAfter = arr[indexOfLargest+1];
-    if (numberBefore > numberAfter) {
-        return numberBefore
-    } else {
-        return numberAfter
-    };
+for (let i = 0; i < numbers.length; i++) {
+    var currentNum = numbers[i];
+    var nextNum = numbers[i + 1];
+    var product = currentNum * nextNum;
+
+    if (product) {
+        products.push(product);
+    }
     
-    // return adjacentNumbers;
-};
+}
+var largestProduct = Math.max(...products);
 
-// multiply those two elements together and return
-function getResult(a, b) {
-    return multiplier * multiplicand;
-};
-
-
-console.log(findLargestElement(arrOfInts, (arrOfInts.length - 1)));
-// console.log(arrOfInts);
-largestAdjascentElement = findLargestAdjascentElement(arrOfInts);
-var multiplicand = largestAdjascentElement
-console.log(getResult(multiplier, multiplicand));
+console.log(largestProduct);
